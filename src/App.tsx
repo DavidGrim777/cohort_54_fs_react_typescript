3a844adac506b897c2f07f6e9b38c961c5bbd30e
DavidGrim777 <davidgrim676@gmail.com>
2025-08-17 00:12:59 +0200

    Add Lesson 07 and adding changes to homework 06

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,8 @@
 // //Lessons
 // import Lesson_06 from "./lessons/Lesson_06/Lesson_06";
+import Lesson_07 from "./lessons/Lesson_07/Lesson_07";
+
+// Homeworks
 import Homework_06 from "./homeworks/Homework_06/Homework_06";
 
 import "./App.css";
@@ -9,9 +12,10 @@ function App() {
     <div className="app">
       {/* Lessons */}
       {/* <Lesson_06 /> */}
+      <Lesson_07 />
 
       {/* Homeworks */}
-      <Homework_06 />
+      {/* <Homework_06 /> */}
     </div>
   );
 }
