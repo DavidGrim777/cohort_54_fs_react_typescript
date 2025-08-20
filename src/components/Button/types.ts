509983c1deeaf605120b49394ba5e0b874d95910
DavidGrim777 <davidgrim676@gmail.com>
2025-08-20 19:01:45 +0200

    Add Lesson 08

--- src/components/Button/types.ts
+++ src/components/Button/types.ts
@@ -1,9 +1,23 @@
+// import { type ReactNode } from "react";
+
+// export type ButtonType = "button" | "submit" | "reset" | undefined;
+
+// export interface ButtonProps {
+//   type?: ButtonType;
+//   name: string;
+//   onClick?: () => void;
+//   children?: ReactNode;
+// }
+
+
 import { type ReactNode } from "react";
 
 export type ButtonType = "button" | "submit" | "reset" | undefined;
 
 export interface ButtonProps {
   type?: ButtonType;
+  isRed?: boolean;
+  disabled?: boolean;
   name: string;
   onClick?: () => void;
   children?: ReactNode;
