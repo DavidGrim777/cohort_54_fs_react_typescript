2a73b2a0fa896a94d210a0b185c9220d0746b585
DavidGrim777 <davidgrim676@gmail.com>
2025-08-30 22:22:09 +0200

    Add Lesson 10 and 11, and add Homework 10

--- src/components/Button/styles.ts
+++ src/components/Button/styles.ts
@@ -30,14 +30,14 @@
   font-weight: bold;
   cursor: pointer;
 } */
-
-/* 
+import styled from "@emotion/styled";
+export const ButtonComponent = styled.div`
   display: flex;
   align-items: center;
   justify-content: center;
   outline: none;
   width: 100%;
-  height: 70px;
+  height: 50px;
   background-color: rgb(84, 84, 247);
   color: white;
   border: none;
@@ -46,64 +46,68 @@
   font-size: 16px;
   font-weight: 500;
   font-family: "Lato", sans-serif;
-}
-
-.button_component:hover {
-  background-color: rgb(86, 86, 236);
+  
+  
+ &:hover{
+   background-color: rgb(86, 86, 236);
   box-shadow: 0px 0px 8px gray;
-} */
-
-import styled from "@emotion/styled";
-
-interface ButtonComponentProps {
-  $isRed: boolean;
-}
-
-const generateButtonColor = (isRed: boolean, disabled: boolean | undefined) => {
-  if (disabled) {
-    return "#acacacff";
-  } else {
-    if (isRed) {
-      return "#ff6868ff";
-    } else {
-      return "#1f27f5";
-    }
   }
-};
+`
 
-const generateButtonColorOnHover = (
-  isRed: boolean,
-  disabled: boolean | undefined
-) => {
-  if (disabled) {
-    return "#acacacff";
-  } else {
-    if (isRed) {
-      return "#fc3333ff";
-    } else {
-      return "rgb(97, 102, 255)";
-    }
-  }
-};
 
-export const ButtonComponent = styled.button<ButtonComponentProps>`
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  outline: none;
-  border: none;
-  padding: 0;
-  height: 70px;
-  width: 100%;
-  background-color: ${({ $isRed, disabled }) =>
-    generateButtonColor($isRed, disabled)};
-  color: #ffffff;
-  font-size: 20px;
-  font-weight: bold;
-  cursor: ${({ disabled }) => (disabled ? "not-allowed" : "pointer")};
 
-  &:hover {
-    background-color: ${({ $isRed, disabled }) =>
-      generateButtonColorOnHover($isRed, disabled)};
-  }
-`;
+
+
+
+// interface ButtonComponentProps {
+//   $isRed: boolean;
+// }
+
+// const generateButtonColor = (isRed: boolean, disabled: boolean | undefined) => {
+//   if (disabled) {
+//     return "#acacacff";
+//   } else {
+//     if (isRed) {
+//       return "#ff6868ff";
+//     } else {
+//       return "#1f27f5";
+//     }
+//   }
+// };
+
+// const generateButtonColorOnHover = (
+//   isRed: boolean,
+//   disabled: boolean | undefined
+// ) => {
+//   if (disabled) {
+//     return "#acacacff";
+//   } else {
+//     if (isRed) {
+//       return "#fc3333ff";
+//     } else {
+//       return "rgb(97, 102, 255)";
+//     }
+//   }
+// };
+
+// export const ButtonComponent = styled.button<ButtonComponentProps>`
+//   display: flex;
+//   align-items: center;
+//   justify-content: center;
+//   outline: none;
+//   border: none;
+//   padding: 0;
+//   height: 70px;
+//   width: 100%;
+//   background-color: ${({ $isRed, disabled }) =>
+//     generateButtonColor($isRed, disabled)};
+//   color: #ffffff;
+//   font-size: 20px;
+//   font-weight: bold;
+//   cursor: ${({ disabled }) => (disabled ? "not-allowed" : "pointer")};
+
+//   &:hover {
+//     background-color: ${({ $isRed, disabled }) =>
+//       generateButtonColorOnHover($isRed, disabled)};
+//   }
+// `;
